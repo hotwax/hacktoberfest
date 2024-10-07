5307709c1e698b83f3286a731d05f4ee03fa080b
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2024-10-07 08:36:15 +0530

    Update run_Service_generator.py

--- Ayan_Project4/Ayan_project4/run_Service_generator.py
+++ Ayan_Project4/Ayan_project4/run_Service_generator.py
@@ -1,18 +1,18 @@
 # %%writefile run_service_generator.py
-from Service_generator import ServiceGenerator
+# from Service_generator import ServiceGenerator
 
-def main():
-    generator = ServiceGenerator()
-    request_text = input("Enter your service request: ")
-    business_logic = input("Enter business logic (if any): ")
+# def main():
+#     generator = ServiceGenerator()
+#     request_text = input("Enter your service request: ")
+#     business_logic = input("Enter business logic (if any): ")
 
-    if business_logic:
-        service_code = generator.generate_service_code_with_complex_logic(request_text, business_logic)
-    else:
-        service_code = generator.generate_service_code(request_text)
+#     if business_logic:
+#         service_code = generator.generate_service_code_with_complex_logic(request_text, business_logic)
+#     else:
+#         service_code = generator.generate_service_code(request_text)
 
-    print("\nGenerated Service Code:\n")
-    print(service_code)
+#     print("\nGenerated Service Code:\n")
+#     print(service_code)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
